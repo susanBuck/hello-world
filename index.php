567c31cadc2447eec2c5dc245c32b305125c7b80
Susan Buck <susan@susanbuck.net>
2019-02-05 20:54:05 -0500

    Added homepage with ice cream image

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+
+    <title>Hello World</title>
+    <meta charset='utf-8'>
+
+</head>
+<body>
+
+    <h1>Hello World</h1>
+    <img src='/images/icecream.jpg' alt='Ice cream in cup with sprinkles'>
+    
+</body>
+</html>
